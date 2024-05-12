c2ff604bffe8c35ce459c237e741b9394a65ef2b
mhmad-nr <83580680+mhmad-nr@users.noreply.github.com>
2024-05-12 10:52:30 +0330

    init

---
+++ contracts/ImplementationV1.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+contract ImplementationV1 {
+    
+    uint256 public val;
+
+    function add() public {
+        val += 1;
+    }
+}
